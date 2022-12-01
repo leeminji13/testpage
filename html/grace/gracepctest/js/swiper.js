900ee224599da6e663023dc3f61e5bd4d592b5ab
leeminji <minjis2s2s2@gmail.com>
2022-12-01 13:59:47 +0900

    모바일 슬라이드 텍스트 수정,

---
+++ html/grace/gracepctest/js/swiper.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper("#sec4_slide", {
+    slidesPerView: "auto",
+    loop : true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  var swiper1 = new Swiper("#sec4_sideSld", {
+    slidesPerView: "auto",
+    loop : true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
